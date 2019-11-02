2ac05de98296a0a8369d0de099949035a9eb01e0
PulakChandan <pulak.pc@gmail.com>
2019-11-02 10:10:42 +0530

    test

--- Python_project/DSUP/DS/LinkedList.py
+++ Python_project/DSUP/DS/LinkedList.py
@@ -1,26 +1,26 @@
                                  
 """Linked List"""
 class Node:
-    def __init__(self,data):
-        self.__data=data
-        self.__next=None
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
 
     def get_data(self):
         return self.__data
     
-    def set_data(self,data):
+    def set_data(self, data):
         self.__data=data
     
     def get_next(self):
         return self.__next
     
-    def set_next(self,next_node):
-        self.__next=next_node
+    def set_next(self, next_node):
+        self.__next = next_node
     
 class LinkedList:
     def __init__(self):
-        self.__head=None
-        self.__tail=None
+        self.__head = None
+        self.__tail = None
     
     def get_head(self):
         return self.__head
